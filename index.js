06a3491c4e8b703727221b900f7a75044221c68d
Tyler Cummings <tylercummings@comcast.net>
2020-03-22 14:32:00 -0400

    Added messaging for amount of time a user  was spent AFK

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 const Discord = require('discord.js')
 const { botId, token } = require('./config.json')
+const countdown = require('countdown')
 
 const FILE_PATH = './cuban-pete.mp3'
 
 const client = new Discord.Client()
+const COUNTDOWN_UNITS = countdown.HOURS | countdown.MINUTES | countdown.SECONDS
+const afkTimes = {}
 
 client.login(token)
   .then(() => console.log('Started'))
 
 client.on('error', console.error)
 
-client.on('voiceStateUpdate', (_, { voiceChannel }) => {
-  if (isAfkChannel(voiceChannel) && !isBotInChannel(voiceChannel)) {
-    voiceChannel.join()
-      .then(connection => {
-        unmuteBotInAfkChannel(voiceChannel)
-        playFileIndefinitely(connection)
-      })
-    }
-  })
+client.on('voiceStateUpdate', (oldGuildMember, newGuildMember) => {
+  if (newGuildMember.user.bot || oldGuildMember.user.bot) return
+
+  if (isAfkChannel(newGuildMember.voiceChannel)) {
+    respondToAfkChannelJoin(newGuildMember)
+  } else if (isAfkChannel(oldGuildMember.voiceChannel) && !oldGuildMember.user.bot) {
+    sendAfkTime(oldGuildMember)
+  }
+})
+
+const respondToAfkChannelJoin = ({ voiceChannel, user }) => {
+  afkTimes[user.id] = new Date()
+  if (!isBotInChannel(voiceChannel)) {
+    joinChannelAndPlayIndefinitely(voiceChannel)
+  }
+}
+
+const joinChannelAndPlayIndefinitely = voiceChannel => {
+  voiceChannel.join()
+    .then(connection => {
+      unmuteBotInAfkChannel(voiceChannel)
+      playFileIndefinitely(connection)
+    })
+}
 
 const playFileIndefinitely = connection => {
-  var dispatcher = connection.playFile(FILE_PATH)
+  const dispatcher = connection.playFile(FILE_PATH)
   dispatcher.on('end', () => {
     if (connection.channel.members.array().length > 1) {
       playFileIndefinitely(connection)
@@ -29,6 +47,17 @@ const playFileIndefinitely = connection => {
       setTimeout(() => connection.disconnect(), 1000)
     }
   })
+}
+
+const sendAfkTime = ({ guild, user }) => {
+  const afkStartTime = afkTimes[user.id]
+  if (!afkStartTime) return
+
+  const totalAFKTime = countdown(afkStartTime, new Date(), COUNTDOWN_UNITS)
+  delete afkTimes[user.id]
+
+  guild.channels.find(({ type }) => type === 'text')
+    .send(`You were Cuban Pete'd for ${totalAFKTime}`, { reply: user })
 }
 
 const isAfkChannel = voiceChannel =>
