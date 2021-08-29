06119925f2c4edffce18a3927d2f07800d80f2a2
Tyler Cummings <tylercummings@comcast.net>
2021-08-29 15:41:56 -0400

    Update discord.js to v12

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
-const { botId, token } = require('./config.json')
 const countdown = require('countdown')
 const Database = require('./database')
+const { botId, token } = require('./config.json')
 
 const FILE_PATH = './cuban-pete.mp3'
 
@@ -14,38 +14,38 @@ client.login(token)
 
 client.on('error', console.error)
 
-client.on('voiceStateUpdate', (oldGuildMember, newGuildMember) => {
-  const { user: oldUser, voiceChannel: oldVoiceChannel } = oldGuildMember
-  const { user: newUser, voiceChannel: newVoiceChannel } = newGuildMember
+client.on('voiceStateUpdate', async (oldState, newState) => {
+  const oldGuildMember = oldState.member
+  const oldVoiceChannel = oldState.channel
+  const newGuildMember = newState.member
+  const newVoiceChannel = newState.channel
 
-  if (newUser.bot || oldUser.bot || newVoiceChannel === oldVoiceChannel) return
+  if (newGuildMember.user.bot || oldGuildMember.user.bot || newVoiceChannel === oldVoiceChannel) return
 
   if (isAfkChannel(newVoiceChannel)) {
-    respondToAfkChannelJoin(newGuildMember)
-  } else if (isAfkChannel(oldVoiceChannel) && !oldUser.bot) {
-    sendAfkTime(oldGuildMember)
+    await respondToAfkChannelJoin(newGuildMember)
+  } else if (isAfkChannel(oldVoiceChannel) && !oldGuildMember.user.bot) {
+    await sendAfkTime(oldGuildMember)
   }
 })
 
-const respondToAfkChannelJoin = ({ voiceChannel, guild, user }) => {
+const respondToAfkChannelJoin = async ({ voice, guild, user }) => {
   db.startAfkSessionForUser({ userId: user.id, guildId: guild.id, timeEntered: new Date() })
-  if (!isBotInChannel(voiceChannel)) {
-    joinChannelAndPlayIndefinitely(voiceChannel)
+  if (!isBotInChannel(voice.channel)) {
+    await joinChannelAndPlayIndefinitely(voice.channel)
   }
 }
 
-const joinChannelAndPlayIndefinitely = voiceChannel => {
-  voiceChannel.join()
-    .then(connection => {
-      unmuteBotInAfkChannel(voiceChannel)
-      playFileIndefinitely(connection)
-    })
+const joinChannelAndPlayIndefinitely = async voiceChannel => {
+  const connection = await voiceChannel.join()
+  unmuteBotInAfkChannel(voiceChannel)
+  playFileIndefinitely(connection)
 }
 
 const playFileIndefinitely = connection => {
-  const dispatcher = connection.playFile(FILE_PATH)
-  dispatcher.on('end', () => {
-    if (connection.channel.members.array().length > 1) {
+  const dispatcher = connection.play(FILE_PATH)
+  dispatcher.on('finish', () => {
+    if (connection.channel.members.size > 1) {
       playFileIndefinitely(connection)
     } else {
       setTimeout(() => connection.disconnect(), 1000)
@@ -53,42 +53,43 @@ const playFileIndefinitely = connection => {
   })
 }
 
-const sendAfkTime = ({ guild, user }) => {
-  var id = db.endAfkSessionForUser({ userId: user.id, timeExited: new Date(), guildId: guild.id })
+const sendAfkTime = async ({ guild, user }) => {
+  const id = db.endAfkSessionForUser({ userId: user.id, timeExited: new Date(), guildId: guild.id })
 
   const { timeEntered, timeExited } = db.getAfkSessionById(id)
   const totalAFKTime = countdown(timeEntered, timeExited, COUNTDOWN_UNITS)
 
-  const textChannels = guild.channels.filter(({ type }) => type === 'text')
+  const textChannels = guild.channels.cache.filter(({ type }) => type === 'text')
   const generalChannel = textChannels.find(({ name }) => name.includes('general')) || textChannels.first()
 
-  generalChannel.send(`You were Cuban Pete'd for ${totalAFKTime.toString()}`, { reply: user })
-  assignRole(guild, generalChannel)
+  await generalChannel.send(`You were Cuban Pete'd for ${totalAFKTime.toString()}`, { reply: user })
+  await assignRole(guild, generalChannel)
 }
 
-const assignRole = (guild, channel) => {
-  var [longestAfkSession] = db.getLongestAfkSessionsForGuild(guild.id, 1)
-  var roleId = db.getHighScoreRoleForGuild(guild.id)
+const assignRole = async (guild, channel) => {
+  let [longestAfkSession] = db.getLongestAfkSessionsForGuild(guild.id, 1)
+  let roleId = db.getHighScoreRoleForGuild(guild.id)
   if (!roleId) {
-    guild.createRole({
-      name: 'Cuban Pete World Champion',
-      hoist: true,
-      mentionable: true
-    }).then(role => {
-      db.setHighScoreRoleForGuild(guild.id, role.id)
-      var member = guild.members.find(({ id }) => id === longestAfkSession.userId)
-      member.addRole(role.id)
-      channel.send(`<@${member.id}> has been declared <@&${role.id}>!`)
+    const role = await guild.roles.create({
+      data: {
+        name: 'Cuban Pete World Champion',
+        hoist: true,
+        mentionable: true
+      }
     })
 
+    db.setHighScoreRoleForGuild(guild.id, role.id)
+    let member = guild.members.cache.find(({ id }) => id === longestAfkSession.userId)
+    await member.roles.add(role.id)
+    await channel.send(`<@${member.id}> has been declared <@&${role.id}>!`)
   } else {
-    var role = guild.roles.find(role => role.id === roleId)
-    var previousChampion = role.members.first()
+    let role = guild.roles.cache.find(role => role.id === roleId)
+    let previousChampion = role.members.first()
     if (previousChampion.id !== longestAfkSession.userId) {
-      previousChampion.removeRole(role.id)
-      var currentChampion = guild.members.find(({ id }) => id === longestAfkSession.userId)
-      currentChampion.addRole(role.id)
-      channel.send(`<@${currentChampion.id}> has claimed the title of <@&${role.id}> from <@${previousChampion.id}>!`)
+      previousChampion.roles.remove(role.id)
+      let currentChampion = guild.members.cache.find(({ id }) => id === longestAfkSession.userId)
+      await currentChampion.roles.add(role.id)
+      await channel.send(`<@${currentChampion.id}> has claimed the title of <@&${role.id}> from <@${previousChampion.id}>!`)
     } else {
       return { championId: previousChampion.id, roleId }
     }
@@ -96,7 +97,7 @@ const assignRole = (guild, channel) => {
 }
 
 const isAfkChannel = voiceChannel =>
-  !!voiceChannel && client.guilds.some(({ afkChannelID }) => afkChannelID === voiceChannel.id)
+  !!voiceChannel && client.guilds.cache.some(({ afkChannelID }) => afkChannelID === voiceChannel.id)
 
 const isBotInChannel = voiceChannel =>
   !!getBotMembersInChannel(voiceChannel).array().length
@@ -105,15 +106,15 @@ const getBotMembersInChannel = ({ members }) =>
   members.filter(({ user }) => user.bot && user.id === botId)
 
 const unmuteBotInAfkChannel = voiceChannel =>
-  getBotMembersInChannel(voiceChannel).forEach(member => member.setMute(false))
+  getBotMembersInChannel(voiceChannel).forEach(member => member.voice.setMute(false))
 
-client.on('message', ({ guild, content, channel }) => {
+client.on('message', async ({ guild, content, channel }) => {
   if (content === '!leaderboard') {
     const NUMBER_OF_SESSIONS = 5
 
-    var afkSessions = db.getLongestAfkSessionsForGuild(guild.id, NUMBER_OF_SESSIONS)
+    let afkSessions = db.getLongestAfkSessionsForGuild(guild.id, NUMBER_OF_SESSIONS)
 
-    var embed = {
+    const embed = {
       color: 6875242,
       title: `Top ${NUMBER_OF_SESSIONS} Cuban Petes for ${guild.name}`,
       fields: afkSessions.map(({ userId, totalTime }, i) => ({
@@ -122,11 +123,11 @@ client.on('message', ({ guild, content, channel }) => {
       }))
     }
 
-    channel.send({ embed })
+    await channel.send({ embed })
   } else if (content === '!highscore') {
-    var { championId, roleId } = assignRole(guild, channel) || {}
+    let { championId, roleId } = await assignRole(guild, channel) || {}
     if (championId) {
-      channel.send(`<@${championId}> is the <@&${roleId}>!`)
+      await channel.send(`<@${championId}> is the <@&${roleId}>!`)
     }
   }
 })
